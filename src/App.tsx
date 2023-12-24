83caf6d1745464fc43a7930cda6a1af4ea5eaf01
정효진 <jeonghyojin@keunhyojing.local>
2023-12-24 22:58:43 +0900

    loading page ui 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,52 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import styled, { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import Loading from "./components/loading";
+import '@picocss/pico';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Loading />,
+    children: [
+      {
+        path: "/home",
+        //element: <Home />,
+      },
+      {
+        path: "/mypage",
+        //element: <Mypage />,
+      },
+      {
+        path: "/place",
+        //element: <Place />,
+      },
+      {
+        path: "/search",
+        //element: <Search />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    //element: <Login />,
+  },
+  {
+    path: "/create-account",
+    //element: <CreateAccount />,
+  },
+]);
+
+const Main = styled.main`
+  text-align: center;
+`;
+
 function App() {
-  return <></>;
+  return (
+    <Main className='container'>
+      <RouterProvider router={router} />
+    </Main>
+  );
 }
 
 export default App;
