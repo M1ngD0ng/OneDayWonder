7f76ee2ba393449e0c1f3257e3d8b8987867a2a7
alswjd6807@naver.com <alswjd6807@naver.com>
2023-12-29 19:19:59 +0900

    like edit

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -59,7 +59,7 @@ export default function NavBar() {
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"></path>
     </svg></div>
       </NavLink>
-      <NavLink to="/home">
+      <NavLink to="">
         <div><svg data-slot="icon" fill="none" stroke-width="1.5"  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
   </svg></div>
--- src/routes/like.tsx
+++ src/routes/like.tsx
@@ -1,5 +1,67 @@
+import styled from "styled-components"
+
+ 
+ const Wrapper=styled.div`
+    height: 100vh;
+    flex-direction: column;
+    display: flex;
+ `;
+
+const Upper=styled.div`
+  justify-content: center;
+`
+const Lower=styled.div`
+  ::-webkit-scrollbar {
+      display:none;
+    }
+`
+const Info=styled.div`
+  margin-top: 20%;
+  font-size: 25px;
+`;
+const Name=styled.span`
+  font-weight: bold;
+`;
+
+const Order=styled.div`
+  margin-top: 10%;
+  button{
+    color: black;
+    font-size: 16px;
+    width: max-content;
+    float: right;
+    margin-left: 1%;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    background-color: inherit;
+  }
+`;
+
+const OrderByDay=styled.div`
+  div{
+    margin-left: 5%;
+    float: left;
+  }
+`;
 export default function Like(){
   return (
-    <div>like!</div>
+    <Wrapper>
+      <Upper>
+        <Info>
+          <Name>이민정</Name>&nbsp;
+          <span>님의 "To Go List"</span>
+        </Info>
+        <Order>
+          <button>카테고리별</button>
+          <button>등록순</button>
+        </Order>
+      </Upper>
+      <Lower>
+        <OrderByDay>
+          <div>2023년 12월 13일</div>
+        </OrderByDay>
+      </Lower>
+    </Wrapper>
   )
 }
